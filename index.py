74262a3bda337a523c7d6b98d8769540bb2104e2
Bart van Sevenbergen <bartvansevenbergen@hotmail.com>
2021-06-15 11:11:24 +0200

    test for laptop

--- index.py
+++ index.py
@@ -5,6 +5,7 @@
 import cv2
 import numpy as np
 from time import time
+import serial
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
@@ -12,60 +13,68 @@ if not cap.isOpened():
     exit()
 
 
-def calc_white_pixels():
-    # Our operations on the frame come here
+def calc_factor(frame):
+    white = calc_white_pixels(frame)
+    total_pixels = frame.shape[0] * frame.shape[1]
+    factor = (white/total_pixels) * 100
+    return factor
+
+
+def get_frame_grayscale():
     ret, frame = cap.read()
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         return -1
-
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # ret, thresh1 = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY)
-
     # Display the resulting frame
     lower_flashlight = np.array([0, 0, 255])
     upper_flashlight = np.array([179, 255, 255])
     mask = cv2.inRange(hsv, lower_flashlight, upper_flashlight)
     res = cv2.bitwise_and(frame, frame, mask=mask)
-    white_pixels = np.sum(res == 255)
-    cv2.imshow('frame', res)
+    return res
+
+
+def calc_white_pixels(frame):
+    white_pixels = np.sum(frame == 255)
     return white_pixels
 
 
-def calibrate():
+def calibrate(frame):
     amount = 20
     pixel_count = 0
     for count in range(amount):
-        pixel_count += calc_white_pixels()
+        pixel_count += calc_white_pixels(frame)
     avg = pixel_count // amount
-    thresh = avg + 500
-    print(thresh)
+    thresh = avg + 1000
+    print("done calibrating.")
     return int(thresh)
 
 
 prev_time = time()
 
 
-def process():
-    white_pixels = calc_white_pixels()
+def process(frame):
+    white_pixels = calc_white_pixels(frame)
     interval = 5
     global prev_time
     time_elapsed = time() - prev_time
-    # print(white_pixels)
     if white_pixels >= threshold:
         prev_time = time()
-        print("exceeded", prev_time)
+        print(calc_factor(frame))
     #     do light stuff
-    elif time() - prev_time > interval:
+    elif time_elapsed > interval:
         print("wuuutt")
         prev_time = time()
-    #   do light slow down effects
+    #   do slow down effects
 
 
 if __name__ == "__main__":
-    threshold = calibrate()
+    threshold = calibrate(get_frame_grayscale())
     while True:
-        process()
+        frame = get_frame_grayscale()
+        process(frame)
+        cv2.imshow('frame', frame)
+
         if cv2.waitKey(1) == ord('q'):
             break
         # When everything done, release the capture
